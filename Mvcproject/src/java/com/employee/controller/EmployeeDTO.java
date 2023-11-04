942436eb33121f98c44d099a452620296f32a2dd
Uday Mourya <136227669+uday-mourya@users.noreply.github.com>
2023-11-04 21:30:34 +0530

    4/11/2023

---
+++ Mvcproject/src/java/com/employee/controller/EmployeeDTO.java
@@ -0,0 +1,10 @@
+
+package com.employee.controller;
+
+/**
+ *
+ * @author dell
+ */
+public class EmployeeDTO {
+    
+}
---
+++ Web/html11/cssgav/humbergericon/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+  }
+  
